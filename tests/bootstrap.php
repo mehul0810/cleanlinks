c8dfd8fd206b355cca83e5b6363a45b5e37249f4
Ankur Modi <ankurmodi0812@gmail.com>
2024-04-05 21:02:23 +0530

    Create new structure for phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,29 +1,25 @@
 <?php
-/**
- * PHPUnit bootstrap file.
- *
- * @package FewerTags
- */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+//namespace SimplifiedWP\Links;
 
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+use SimplifiedWP\Links\Tests\TestEnvironment;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+const WP_CONTENT_DIR = __DIR__;
+
+$testEnvironment = new TestEnvironment();
+
+// check if a required `wp-tests-config.php` is present
+if (!$testEnvironment->hasConfig()) {
+    die('wp-tests-config.php not found');
 }
 
-// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
-$_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
-if ( false !== $_phpunit_polyfills_path ) {
-	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
-}
+// get the current test environment (Local or Workflow)
+$currentTestEnvironment = $testEnvironment->current();
 
-if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
-	echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
-
-// Give access to tests_add_filter() function.
-require_once "{$_tests_dir}/includes/functions.php";
+// define for use in WP bootstrap file
+define('WP_TESTS_CONFIG_FILE_PATH', $currentTestEnvironment->config());
 
 /**
  * Manually load the plugin being tested.
@@ -34,5 +30,13 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
-require "{$_tests_dir}/includes/bootstrap.php";
+// install GiveWP
+tests_add_action('setup_theme', static function () {
+    echo 'Installing SimplifiedWP.....' . PHP_EOL;
+    // Initialize the plugin.
+    $plugin = new Plugin();
+    $plugin->register();
+});
+
+// pull in WP bootstrap file which looks for WP_TESTS_CONFIG_FILE_PATH defined above
+require_once $currentTestEnvironment->bootstrap();
