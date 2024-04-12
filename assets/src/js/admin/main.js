696318f818fcb1f4d42226418de0175e42b3bb38
Mehul Gohil <mehul.gohil@get10up.com>
2024-04-12 19:48:11 +0530

    improved formatting

--- assets/src/js/admin/main.js
+++ assets/src/js/admin/main.js
@@ -1,116 +1,123 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 	// Get all elements with the class js-simplified-link-button
-    const copyButtons = document.querySelectorAll(".js-simplified-link-button");
+	const copyButtons = document.querySelectorAll(
+		'.js-simplified-link-button'
+	);
 
-    const migratebtn = document.getElementById('migrate-btn');
-    const errorMessage = document.getElementById('errormessage');
-    const progress = document.getElementById('progress');
-    const progressBar = document.getElementById('progressbar');
-    const progressmsg = document.querySelector(".js-message");
+	const migratebtn = document.getElementById( 'migrate-btn' );
+	const errorMessage = document.getElementById( 'errormessage' );
+	const progress = document.getElementById( 'progress' );
+	const progressBar = document.getElementById( 'progressbar' );
+	const progressmsg = document.querySelector( '.js-message' );
 
-    const setProgressZero = () => {
-        progressBar.style.width = '0%'; // Initialize progress bar width to 0%
-        progress.classList.remove('sl-hidden'); // Show progress
-        progressmsg.textContent = "Importing and updating your links. For large bulk imports this may take some time with no progress bar movement.";
-    }
+	const setProgressZero = () => {
+		progressBar.style.width = '0%'; // Initialize progress bar width to 0%
+		progress.classList.remove( 'sl-hidden' ); // Show progress
+		progressmsg.textContent =
+			'Importing and updating your links. For large bulk imports this may take some time with no progress bar movement.';
+	};
 
-    const setProgress = (progessPercentage) => {
-        progressBar.style.width = progessPercentage + '%';
-    }
+	const setProgress = ( progessPercentage ) => {
+		progressBar.style.width = progessPercentage + '%';
+	};
 
-    const hideProgressBar = () => {
-        progressmsg.textContent = "";
-        progress.classList.add('sl-hidden'); // Hide progress bar
-    };
+	const hideProgressBar = () => {
+		progressmsg.textContent = '';
+		progress.classList.add( 'sl-hidden' ); // Hide progress bar
+	};
 
-    if ( null !== migratebtn ) {
-		migratebtn.addEventListener( 'click', e => {
+	if ( null !== migratebtn ) {
+		migratebtn.addEventListener( 'click', ( e ) => {
 			e.preventDefault();
 
-            setProgressZero(); // Show progress bar before sending the request
-        
-            const formData = new FormData();
+			setProgressZero(); // Show progress bar before sending the request
+
+			const formData = new FormData();
 			formData.append( 'action', 'simplified_import' );
 
-            fetch(
-                ajaxurl, 
-                {
-					method: 'POST',
-					body: formData,
-				}
-            ).then(response => {
-                setProgress(10);
-                if ( 200 === response.status ) {
-                    return response.json();
-                }
-                return false; 
-            } ).then( data => {
-                console.log("data = "+data.success);
-                if( data.success === true ) {
-                    let width = 0;
-                    const interval = setInterval(() => {
-                        width += 10; // Increase width by 10%
-                        progressBar.style.width = `${width}%`;
-                
-                        if (width >= 100) {
-                            clearInterval(interval); // Stop the interval when width reaches 100%
-                            progressmsg.textContent = "Import Successfully";
-                        }
-                    }, 200); // Update progress every 200 milliseconds
-                setTimeout(() => {
-                        hideProgressBar();
-                    }, 5000);
-                    console.log("success");    
-                } else {
-                    console.log("data = "+data.success);
-                    console.log("error");
-                    errorMessage.classList.remove('sl-hidden');
-                    errorMessage.textContent = "There was no any import lasso post data.";
-                    setTimeout(() => {
-                        errorMessage.classList.add('sl-hidden');
-                        errorMessage.textContent = "";
-                        hideProgressBar();
-                    }, 5000);
-                }
-            } )
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            } );        
-        })
-    }
-    
-	copyButtons.forEach(function (btn) {
-        const defaultText = btn.getAttribute('data-default-text');
-		const copiedText = btn.getAttribute('data-copied-text');
-		
-		btn.addEventListener('click', function () {
-            var simplifiedUrl = this.getAttribute('data-url');
-			const span = btn.querySelector('.dashicons');
+			fetch( ajaxurl, {
+				method: 'POST',
+				body: formData,
+			} )
+				.then( ( response ) => {
+					setProgress( 10 );
+					if ( 200 === response.status ) {
+						return response.json();
+					}
+					return false;
+				} )
+				.then( ( data ) => {
+					console.log( 'data = ' + data.success );
+					if ( data.success === true ) {
+						let width = 0;
+						const interval = setInterval( () => {
+							width += 10; // Increase width by 10%
+							progressBar.style.width = `${ width }%`;
+
+							if ( width >= 100 ) {
+								clearInterval( interval ); // Stop the interval when width reaches 100%
+								progressmsg.textContent = 'Import Successfully';
+							}
+						}, 200 ); // Update progress every 200 milliseconds
+						setTimeout( () => {
+							hideProgressBar();
+						}, 5000 );
+						console.log( 'success' );
+					} else {
+						console.log( 'data = ' + data.success );
+						console.log( 'error' );
+						errorMessage.classList.remove( 'sl-hidden' );
+						errorMessage.textContent =
+							'There was no any import lasso post data.';
+						setTimeout( () => {
+							errorMessage.classList.add( 'sl-hidden' );
+							errorMessage.textContent = '';
+							hideProgressBar();
+						}, 5000 );
+					}
+				} )
+				.catch( ( error ) => {
+					console.error(
+						'There was a problem with the fetch operation:',
+						error
+					);
+				} );
+		} );
+	}
+
+	copyButtons.forEach( function ( btn ) {
+		const defaultText = btn.getAttribute( 'data-default-text' );
+		const copiedText = btn.getAttribute( 'data-copied-text' );
+
+		btn.addEventListener( 'click', function () {
+			var simplifiedUrl = this.getAttribute( 'data-url' );
+			const span = btn.querySelector( '.dashicons' );
 			span.classList.remove( 'dashicons-admin-page' );
-			span.classList.add('dashicons-yes');
+			span.classList.add( 'dashicons-yes' );
 			// Change the button text
-			btn.querySelector('.simplified-button-text').textContent = copiedText;
-			copyTextToClipboard(simplifiedUrl);
-		});
+			btn.querySelector( '.simplified-button-text' ).textContent =
+				copiedText;
+			copyTextToClipboard( simplifiedUrl );
+		} );
 
-		btn.addEventListener('mouseleave', function () {
-            const span = btn.querySelector('.dashicons');
+		btn.addEventListener( 'mouseleave', function () {
+			const span = btn.querySelector( '.dashicons' );
 			span.classList.remove( 'dashicons-yes' );
-			span.classList.add('dashicons-admin-page');
-			btn.querySelector('.simplified-button-text').textContent = defaultText;
-        });
-
-    });
+			span.classList.add( 'dashicons-admin-page' );
+			btn.querySelector( '.simplified-button-text' ).textContent =
+				defaultText;
+		} );
+	} );
 } );
 
-function copyTextToClipboard(text) {
-    var textarea = document.createElement('textarea');
-    textarea.value = text;
-    textarea.setAttribute('readonly', '');
-    textarea.style.position = 'absolute';
-    textarea.style.left = '-9999px';
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
+function copyTextToClipboard( text ) {
+	var textarea = document.createElement( 'textarea' );
+	textarea.value = text;
+	textarea.setAttribute( 'readonly', '' );
+	textarea.style.position = 'absolute';
+	textarea.style.left = '-9999px';
+	document.body.appendChild( textarea );
+	textarea.select();
+	document.execCommand( 'copy' );
+	document.body.removeChild( textarea );
 }
